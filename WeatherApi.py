58699e876e98760007e1191e9876b020f74438e4
unknown <ivgorlik@gmail.com>
2017-12-07 14:01:27 +0200

    Added fixed Weather Api

--- WeatherApi.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q=ashdod')
-json_data = r.json()
-print(json_data)
