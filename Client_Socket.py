1ce353faeb7decabd4b0591570e89cb5571dbc59
Ivgeny Gorelik <34338008+UgeneGorelik@users.noreply.github.com>
2017-12-25 16:02:06 +0200

    Add files via upload

--- Client_Socket.py
+++ Client_Socket.py
@@ -1,23 +1,34 @@
+
+#example of simple Client Socket
+
 import socket
+
+#defining socket details
 
 HOST='localhost'
 PORT=80
 BUFSIZ=256
 
+
+#creatint TCP socket
 client_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock_addr=(HOST,PORT)
+
+#connecting socket
 client_sock.connect(sock_addr)
 payload='GET TIME'
+#sending user defined data encoded in utf-8 to server
+#and recieving data from server
 try:
     while True:
         client_sock.send(payload.encode('utf-8'))
         data =client_sock.recv(BUFSIZ)
         print(repr(data))
-        more = input("Want to send more data to server[y/n]   :")
+        more = input("send data to server?   :")
         if more.lower() == 'y':
-            payload = input("Enter payload: ")
+            payload = input(">>>: ")
         else:
             break
 except KeyboardInterrupt:
-    print('interupted')
+    print('stopped')
 client_sock.close()
